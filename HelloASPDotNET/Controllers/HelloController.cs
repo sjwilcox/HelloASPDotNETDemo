b222d2da0ddfa2e7bbfc35dc5a9c1a3b5996db3d
steven.j.wilcox@gmail.com <steven.j.wilcox@gmail.com>
2021-07-18 11:12:56 -0400

    part 1

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+        }
+    }
+}
